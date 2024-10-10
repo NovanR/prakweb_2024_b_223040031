cb8830466405c3b8205056e3f687e55399dd8ea8
Novan <novanramdan7@gmail.com>
2024-10-10 18:30:54 +0700

    Video 12 - Constant

---
+++ OOP/Video 12/constant.php
@@ -0,0 +1,16 @@
+<?php 
+
+// define("NAMA", "Novan Ramdan");
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 22;
+// echo UMUR;
+
+// class coba {
+//     const NAMA = "Novan";
+// }
+
+// echo coba::NAMA;
+
